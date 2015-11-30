a0a03ff5d72c3c565b8246696fadb48d05d3ff5a
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-30 20:49:05 +0300

    Up

--- src/composing.js
+++ src/composing.js
@@ -41,9 +41,41 @@ import R from 'ramda';
 
 // 3
 // merge
+/*
 {
   const a = Observable.of(1, 2, 3);
   const b = Observable.of(4, 5, 6);
 
   a.merge(b).subscribe(::console.log);
+}
+*/
+
+// 4
+/*
+{
+  const subj1 = new Subject();
+  const subj2 = new Subject();
+
+  subj1.merge(subj2).subscribe(::console.log);
+
+  subj1.onNext('burn');
+  subj2.onNext('motherfucker');
+  subj1.onNext('burn');
+}
+*/
+
+// 5
+{
+  const evens = Observable.of(2, 4, 6, 8);
+  const odds = Observable.of(1, 3, 5, 7, 9);
+
+  const groups = ['even', 'odd'];
+
+  odds.merge(evens)
+    .groupBy(n => n % 2)
+    .subscribe(g => {
+      const group = groups[g.key];
+      g.average().subscribe(avg => console.log(`${group} avg: `, avg));
+      g.subscribe(n => console.log(`${group}: `, n))
+    });
 }
